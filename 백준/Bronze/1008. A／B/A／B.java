0ce38ef4feefb41553ace02834cfdcf5de46584a
wntkdnj33 <122443780+wntkdnj33@users.noreply.github.com>
2023-06-01 18:50:14 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17992 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double A = s.nextDouble();
+        double B = s.nextDouble();
+        double sum = A/B;
+
+        System.out.println(sum);
+    }
+}
