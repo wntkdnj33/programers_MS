1e2fdb6dcacede99cda0c7bdb4502be62a60a77b
wntkdnj33 <122443780+wntkdnj33@users.noreply.github.com>
2023-06-29 14:14:24 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String str = "??!";
+        String str1 = s.next();
+        System.out.println(str1+str);
+    }
+}
