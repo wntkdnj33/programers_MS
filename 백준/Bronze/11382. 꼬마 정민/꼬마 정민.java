deafa789690ff17e080a67e4cab29cddf182a88d
wntkdnj33 <122443780+wntkdnj33@users.noreply.github.com>
2023-06-29 14:35:29 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        long A = s.nextLong();
+        long B = s.nextLong();
+        long C = s.nextLong();
+
+        System.out.println(A+B+C);
+    }
+}
